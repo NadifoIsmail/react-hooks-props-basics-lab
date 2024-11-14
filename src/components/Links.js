8316d90af43992de86c250b54f3b310c452e8229
Nadifo Ismail <nadifoismail80@gmail.com>
2024-11-14 16:25:55 +0300

    modifies about.js,app.js and adds links.js

---
+++ src/components/Links.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Links({github,linkedin}) {
+  return (
+    <div>
+       <h3>Links</h3>
+      {github && (
+        <a href={github} target="_blank" rel="noopener noreferrer">
+          {github}
+        </a>
+      )}
+      <br />
+      {linkedin && (
+        <a href={linkedin} target="_blank" rel="noopener noreferrer">
+          {linkedin}
+        </a>
+      )}
+    </div>
+  )
+}
